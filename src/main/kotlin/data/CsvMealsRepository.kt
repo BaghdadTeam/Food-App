3b943792284628bf2729c859e64716107c4ef1e9
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-04-14 21:43:48 +0200

    implemented the csvMealsRepository.kt

--- src/main/kotlin/data/CsvMealsRepository.kt
+++ src/main/kotlin/data/CsvMealsRepository.kt
@@ -1,10 +1,19 @@
 package org.example.data
 
+import data.CsvReader
+import data.RecordParser
 import model.Meal
 import org.example.logic.MealsRepository
 
-class CsvMealsRepository(): MealsRepository {
+class CsvMealsRepository(
+    private val csvReader: CsvReader,
+    private val recordParser: RecordParser
+): MealsRepository {
     override fun getAllMeals(): List<Meal> {
-        TODO("Not yet implemented")
+        val meals = mutableListOf<Meal>()
+        csvReader.readCsv().forEach { record ->
+            meals.add(recordParser.parseRecord(record))
+        }
+        return meals
     }
 }
