ac2b6f211588bc011388c305e9e6e30bad3524ce
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-04-14 20:10:27 +0200

    modified the models to be nullable

--- src/model/Meal.kt
+++ src/model/Meal.kt
@@ -1,17 +1,16 @@
 package model
 
 data class Meal(
-    val name: String = "",
-    val id: Int = 0,
-    val timeToCock: Int = 0,
-    val contributorId: Int = 0,
-    val date: String = "",
-    val tags: List<String> = emptyList(),
-    val nutrition: Nutrition = Nutrition(),
-    val nSteps: Int = 0,
-    val steps: List<String> = emptyList(),
-    val description: String = "",
-    val ingredients: List<String> = emptyList(),
-    val nIngredients: Int = 0
+    val name: String?,
+    val id: Int?,
+    val timeToCock: Int?,
+    val contributorId: Int?,
+    val date: String?,
+    val tags: List<String>?,
+    val nutrition: Nutrition?,
+    val nSteps: Int?,
+    val steps: List<String?>,
+    val description: String?,
+    val ingredients: List<String>?,
+    val nIngredients: Int?
 )
-
--- src/model/Nutrition.kt
+++ src/model/Nutrition.kt
@@ -1,12 +1,11 @@
 package model
 
 data class Nutrition(
-    val calories: Double = 0.0,
-    val totalFat: Double = 0.0,
-    val sugar: Double = 0.0,
-    val sodium: Double = 0.0,
-    val protein: Double = 0.0,
-    val saturatedFat: Double = 0.0,
-    val carbohydrates: Double = 0.0
+    val calories: Double?,
+    val totalFat: Double?,
+    val sugar: Double?,
+    val sodium: Double?,
+    val protein: Double?,
+    val saturatedFat: Double?,
+    val carbohydrates: Double?
 )
-
