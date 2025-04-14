37687fe740ed6ff5ea1b29b40e3bf573f11fae70
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-04-14 21:50:01 +0200

    created the appModule and useCaseModule for DI

---
+++ src/main/kotlin/di/AppModule.kt
@@ -0,0 +1,16 @@
+package org.example.di
+
+import data.CsvReader
+import data.RecordParser
+import org.example.data.CsvMealsRepository
+import org.example.logic.MealsRepository
+import org.koin.dsl.module
+import java.io.File
+
+val appModule = module {
+    single { File("food.csv") }
+    single { RecordParser() }
+    single { CsvReader(get()) }
+
+    single<MealsRepository> { CsvMealsRepository(get(), get()) }
+}
