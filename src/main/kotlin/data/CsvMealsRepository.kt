434984af7e8e999d832520ddb598f09e931c01ae
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-04-14 21:39:29 +0200

    created the interface for the meals repository

---
+++ src/main/kotlin/data/CsvMealsRepository.kt
@@ -0,0 +1,10 @@
+package org.example.data
+
+import model.Meal
+import org.example.logic.MealsRepository
+
+class CsvMealsRepository(): MealsRepository {
+    override fun getAllMeals(): List<Meal> {
+        TODO("Not yet implemented")
+    }
+}
---
+++ src/main/kotlin/logic/MealsRepository.kt
@@ -0,0 +1,8 @@
+package org.example.logic
+
+import model.Meal
+
+interface MealsRepository {
+
+    fun getAllMeals(): List<Meal>
+}
