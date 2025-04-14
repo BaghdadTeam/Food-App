10b6dfb7cc207190d5d362792678658f5af8385f
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-04-14 19:13:08 +0200

    Created nutrition model

---
+++ src/model/Nutrition.kt
@@ -0,0 +1,12 @@
+package model
+
+data class Nutrition(
+    val calories: Double = 0.0,
+    val totalFat: Double = 0.0,
+    val sugar: Double = 0.0,
+    val sodium: Double = 0.0,
+    val protein: Double = 0.0,
+    val saturatedFat: Double = 0.0,
+    val carbohydrates: Double = 0.0
+)
+
