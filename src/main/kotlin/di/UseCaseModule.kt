92cf3caa183d19d5d095556ca9d2850ebeb1f906
haider <haideraljawasim@gmail.com>
2025-04-15 21:24:54 +0300

    Register multiple meal-related use cases with Koin for DI

--- src/main/kotlin/di/UseCaseModule.kt
+++ src/main/kotlin/di/UseCaseModule.kt
@@ -1,8 +1,15 @@
 package org.example.di
 
-import org.example.logic.FilterQuickHealthyMealsUseCase
+import SuggestHighCalorieMeal
+import org.example.logic.*
 import org.koin.dsl.module
 
 val useCaseModule = module {
     single { FilterQuickHealthyMealsUseCase(get()) }
+    single { SuggestHighCalorieMeal(get()) }
+    single { FilterQuickHealthyMealsUseCase(get()) }
+    single { IraqiMealsIdentifierUseCase(get()) }
+    single { SearchMealUseCase(get()) }
+    single { EggFreeSweetsUseCase(get()) }
+    single { SuggestKetoMealUseCase(get()) }
 }
