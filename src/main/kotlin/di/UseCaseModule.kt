bea65d2db1c6bcc5dad4547df89a1cb7b789e0c1
Youssef Mohammed <youssef.mu.saber@gmail.com>
2025-04-15 20:43:21 +0200

    fixed: add the import for the use cases

--- src/main/kotlin/di/UseCaseModule.kt
+++ src/main/kotlin/di/UseCaseModule.kt
@@ -3,6 +3,10 @@ package org.example.di
 import SuggestHighCalorieMeal
 import org.example.logic.EggFreeSweetsUseCase
 import org.example.logic.FilterQuickHealthyMealsUseCase
+import org.example.logic.IraqiMealsIdentifierUseCase
+import org.example.logic.SearchMealUseCase
+import org.example.logic.EggFreeSweetsUseCase
+import org.example.logic.SuggestHighCalorieMeal
 
 import org.koin.dsl.module
 
