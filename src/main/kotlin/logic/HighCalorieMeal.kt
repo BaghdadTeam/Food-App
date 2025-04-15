0dd6b8233982cc642e8ecc857e47ec672933e96a
Aboud <gh.abd.com.207@gmail.com>
2025-04-15 10:27:21 -0700

    throw an exception when there is no meal with high calorie left

--- src/main/kotlin/logic/HighCalorieMeal.kt
+++ src/main/kotlin/logic/HighCalorieMeal.kt
@@ -4,15 +4,16 @@ import org.example.data.MealsProvider
 class SuggestHighCalorieMeal(private val mealsProvider: MealsProvider) {
     private val suggestedMeals = mutableSetOf<Meal>()
 
-    fun suggestMeal():Meal?{
+    fun suggestMeal(): Meal {
         try {
-
-        return mealsProvider.meals.filter { it.nutrition?.calories!! > 700 && !suggestedMeals.contains(it) }
-            .random().also{suggestedMeals.add(it)}
-
-        }catch (e:NoSuchElementException){
-            print("No more meals to suggest")
-            return null
+            return mealsProvider.meals.filter(::isHighCalorieMeal)
+                .random()
+                .also { suggestedMeals.add(it) }
+        } catch (exception: NoSuchElementException) {
+            throw exception
         }
     }
+
+    fun isHighCalorieMeal(meal: Meal): Boolean =
+        meal.nutrition?.calories!! > 700 && !suggestedMeals.contains(meal)
 }
