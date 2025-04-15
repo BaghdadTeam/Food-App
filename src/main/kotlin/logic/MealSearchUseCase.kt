be5f82d9f16fdeb929013ee509a4bba4b10f420f
Aboud <gh.abd.com.207@gmail.com>
2025-04-15 08:15:28 -0700

    call meals provider dirctly form the method and do or opearation to empty pattern and empty meal name

--- src/main/kotlin/logic/MealSearchUseCase.kt
+++ src/main/kotlin/logic/MealSearchUseCase.kt
@@ -5,20 +5,17 @@ import model.Meal
 import org.example.data.MealsProvider;
 
 class SearchMealUseCase (private val mealsProvider:MealsProvider) {
-    private val allMeals = mealsProvider.meals
 
     fun search(userInput: String): List<Meal> {
         val pattern = userInput.lowercase()
-        return allMeals.filter { findMealUsingKMP(it.name!!.lowercase(), pattern) }
+        return mealsProvider.meals.filter { findMealUsingKMP(it.name!!.lowercase(), pattern) }
 
     }
 
     private fun findMealUsingKMP(mealName: String, pattern: String): Boolean {
         return when {
-            pattern.isEmpty() -> false
-
-            mealName.isEmpty() -> false
-
+            pattern.isEmpty() || mealName.isEmpty() -> false
+            
             else -> kmpSearch(mealName, pattern)
         }
     }
