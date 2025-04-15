6a2087a24c3f53de5861ad203eeb969f28aa4c40
Aboud <gh.abd.com.207@gmail.com>
2025-04-15 08:34:33 -0700

    add second parameter to search meal use case

--- src/main/kotlin/di/UseCaseModule.kt
+++ src/main/kotlin/di/UseCaseModule.kt
@@ -1,12 +1,12 @@
 package org.example.di
 
-import logic.MealSearchUseCase.SearchMealUseCase
 import org.example.logic.FilterQuickHealthyMealsUseCase
 import org.example.logic.IraqiMealsIdentifierUseCase
+import org.example.logic.SearchMealUseCase
 import org.koin.dsl.module
 
 val useCaseModule = module {
     single { FilterQuickHealthyMealsUseCase(get()) }
     single { IraqiMealsIdentifierUseCase(get()) }
-    single { SearchMealUseCase(get()) }
+    single { SearchMealUseCase(get() , get()) }
 }
