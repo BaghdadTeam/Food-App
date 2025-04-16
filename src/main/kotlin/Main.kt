cd84dd1cb3a95517995ab6ba3c6c70dd50f93adb
Dunya4mars <rudina4mars@gmail.com>
2025-04-16 22:57:11 +0300

    add: Print list of easy meals using suggest easy meal use case in cli

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,7 @@ package org.example
 import org.example.di.appModule
 import org.example.di.useCaseModule
 import org.example.logic.FilterQuickHealthyMealsUseCase
+import org.example.logic.SuggestEasyMealUseCase
 import org.koin.core.context.startKoin
 import org.koin.java.KoinJavaComponent.getKoin
 
@@ -14,6 +15,14 @@ fun main() {
 
     val quickHealthyMealsUseCase: FilterQuickHealthyMealsUseCase = getKoin().get()
     val quickHealthyMeals = quickHealthyMealsUseCase.getQuickHealthyMeals(5)
+
+
+    val suggestEasyMealsUseCase: SuggestEasyMealUseCase = getKoin().get()
+    val easyMeals = suggestEasyMealsUseCase.getRandomEasyMeals()
+    println("Easy Meals:")
+    easyMeals.forEachIndexed { index, meal ->
+        println("name${index + 1} - ${meal.name} --->(time: ${meal.timeToCock} mins, ingredients: ${meal.ingredients?.size}, steps: ${meal.steps.size})")
+    }
 
 
 }
