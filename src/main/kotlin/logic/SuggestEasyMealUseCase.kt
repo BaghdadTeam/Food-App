11dc1f4362695bceeb9e3c1f310e4d09e410a9f7
Dunya4mars <rudina4mars@gmail.com>
2025-04-16 22:44:48 +0300

    add: implement logic to filter and return 10 random easy meals

--- src/main/kotlin/logic/SuggestEasyMealUseCase.kt
+++ src/main/kotlin/logic/SuggestEasyMealUseCase.kt
@@ -5,4 +5,16 @@ import org.example.data.MealsProvider
 
 class SuggestEasyMealUseCase(private val mealsRepository: MealsProvider) {
 
+    fun getRandomEasyMeals(): List<Meal> {
+        return mealsRepository.meals
+            .filter(::isEasyMeal)
+            .shuffled()
+            .take(10)
+    }
+
+    private fun isEasyMeal(meal: Meal): Boolean =
+        (meal.timeToCock != null && meal.timeToCock <= 30) &&
+                (meal.ingredients?.size != null && meal.ingredients.size <= 5) &&
+                (meal.steps.size <= 6)
+
 }
