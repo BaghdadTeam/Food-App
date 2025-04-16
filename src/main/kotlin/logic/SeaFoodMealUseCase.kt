849ac55241965656668ed0d6ec314aac8c71d2ad
bassant <bassantelboray@gmail.com>
2025-04-16 18:13:12 +0200

    Modify rankSeafoodMeals and  define val meals and update return logic

--- src/main/kotlin/logic/SeaFoodMealUseCase.kt
+++ src/main/kotlin/logic/SeaFoodMealUseCase.kt
@@ -1,15 +1,16 @@
 package org.example.logic
 
 
+import model.Meal
 import org.example.data.MealsProvider
 
 
 class SeaFoodMealUseCase(
     private val mealsProvider: MealsProvider
 ) {
-    fun rankSeafoodMeals() {
-        val meals=mealsProvider.meals
-        meals.filter { meal ->
+    fun rankSeafoodMeals(): List<Pair<Int, Meal>> {
+        val meals = mealsProvider.meals
+        return meals.filter { meal ->
             meal.tags!!.contains("seafood")
         }.sortedByDescending { it.nutrition!!.protein }.mapIndexed { index, meal ->
             Pair(index + 1, meal)
