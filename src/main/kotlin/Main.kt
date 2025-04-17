d453aa9fcd3c8a7dbed6b973989a19d99648ec93
Abdelrahman Elgendy <abdelrahmanelgendy961@gmail.com>
2025-04-17 18:11:53 +0200

    random-potato-ingredient #12 (modification)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,21 +5,5 @@ import model.Meal
 import java.io.File
 
 fun main() {
-    val csvRecords = CsvReader(File("food.csv")).readCsv()
-    val parser = RecordParser()
-    val meals = mutableListOf<Meal>()
-    csvRecords.forEach { record ->
-        meals.add(parser.parseRecord(record))
-    }
-
-    // println("${meals[5].name?.trim()}: ${meals[5].description}")
-//    val potatoMeal = findPotatoMeals(meals)
-//    displayRandomPotatoMeals(potatoMeal)
-
-    // #8
-//    val searcher = MealDateSearcher()
-//    searcher.searchMealsByDate()
-
-    // #12
 
 }
