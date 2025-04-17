c5eb26334a8e561ce0b01540c441a27f25034258
Narges-Nagy <nargesnagy21@gmail.com>
2025-04-17 20:02:59 +0200

    add use case to explore other countries food culture and use it in UI

--- src/main/kotlin/logic/feature/ExploreOtherCountriesFeature.kt
+++ src/main/kotlin/logic/feature/ExploreOtherCountriesFeature.kt
@@ -1,12 +1,22 @@
 package logic.feature
 
+import logic.use_case.ExploreOtherCountriesFoodCultureUseCase
+
 class ExploreOtherCountriesFeature(
-//    private val useCase: ExploreOtherCountriesUseCase
+    private val useCase: ExploreOtherCountriesFoodCultureUseCase
     ) : Feature {
-    override val number: Int = 10
+    override val number: Int = 20
     override val name: String = "Explore other countries' food culture"
 
     override fun execute() {
-        // Implement your logic here
+        print("Enter country name to discover it's culture ")
+        val countryName = readlnOrNull()?.trim().orEmpty()
+        val meals = useCase.execute(countryName)
+        println(
+            if (meals.isEmpty()) "No meals found matching '$countryName'."
+            else "Matching Meals:\n${
+                meals.joinToString("\n") { "- ${it.name}" }
+            }"
+        )
     }
 }
---
+++ src/main/kotlin/logic/use_case/ExploreOtherCountriesFoodCultureUseCase.kt
@@ -0,0 +1,32 @@
+package logic.use_case
+
+import logic.MealsProvider
+import model.Meal
+
+class ExploreOtherCountriesFoodCultureUseCase(private val mealsProvider: MealsProvider) {
+
+
+    fun execute(countryName: String): List<Meal> {
+        val allMeals = mealsProvider.getMeals()
+            .filter { grtMealsContainInput(it, countryName) }
+            .shuffled().takeIf { it.isNotEmpty() }
+
+        if (allMeals.isNullOrEmpty())  throw NoSuchElementException("No Such element for this country")
+
+        return getNumbersOfMeals(allMeals)
+
+    }
+
+    private fun grtMealsContainInput(meal: Meal, input: String): Boolean {
+        return (meal.name?.contains(input, ignoreCase = true) == true
+                || meal.tags?.any { it.contains(input, ignoreCase = true) } == true
+                || meal.description?.contains(input, ignoreCase = true) == true)
+    }
+
+    private fun getNumbersOfMeals(meals: List<Meal>?): List<Meal> {
+        if (meals.isNullOrEmpty()) return emptyList()
+        return if (meals.size < 20) meals
+        else meals.take(20)
+    }
+
+}
