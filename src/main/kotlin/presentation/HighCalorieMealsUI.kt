20e2ce18e9c45b4bc4d0b6681702387bce6ef35d
Aboud <gh.abd.com.207@gmail.com>
2025-04-18 03:45:40 -0700

    add like or dislike meal , handle if there is no more meal with high calories

--- src/main/kotlin/presentation/HighCalorieMealsUI.kt
+++ src/main/kotlin/presentation/HighCalorieMealsUI.kt
@@ -1,19 +1,46 @@
 package org.example.presentation
 
 import logic.usecase.SuggestHighCalorieMealUseCase
+import model.Meal
 
 class HighCalorieMealsUI(
     private val useCase: SuggestHighCalorieMealUseCase
 ) : Feature {
     override val id: Int = FEATURE_ID
     override val name: String = FEATURE_NAME
-
     override fun execute() {
-        useCase.execute()
+        var likeIt: Boolean
+        var meal: Meal
+        do {
+
+            try {
+                meal = useCase.execute()
+            } catch (exception : NoSuchElementException) {
+                println("There is no more high calorie meals")
+                return
+            }
+
+            println("Meal Name : ${meal.name}")
+            println("Do you like this meal?\n1-yes\n2-no")
+            print("Enter your choice:")
+            val input = readln()
+
+            if (input == "1") {
+                println(meal.description)
+                likeIt = true
+            } else if (input == "2") {
+                likeIt = false
+            } else {
+                break
+            }
+
+        } while (!likeIt)
+
     }
+
 
     companion object {
         const val FEATURE_ID = 13
-        const val FEATURE_NAME = "So thin problem"
+        const val FEATURE_NAME = "Suggest high calorie meal"
     }
 }
