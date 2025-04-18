c48b4beaeb2ba94020d623caefa385fa5dad57b8
haider <haideraljawasim@gmail.com>
2025-04-18 10:31:09 +0300

    separate constant values into a separate companion object

--- src/main/kotlin/logic/use_case/SuggestKetoMealUseCase.kt
+++ src/main/kotlin/logic/use_case/SuggestKetoMealUseCase.kt
@@ -7,7 +7,7 @@ class SuggestKetoMealUseCase(private val mealsProvider: MealsProvider) {
 
     private val alreadySuggestKetoMeals = mutableSetOf<Meal>()
 
-    fun getKetoMealSuggest(): Meal {
+    fun execute(): Meal {
         return mealsProvider.getMeals()
             .filter(::isKetoMealAndNotSuggested)
             .takeIf { it.isNotEmpty() }
@@ -18,9 +18,17 @@ class SuggestKetoMealUseCase(private val mealsProvider: MealsProvider) {
 
     private fun isKetoMealAndNotSuggested(meal: Meal): Boolean {
         val nutrition = meal.nutrition
-        return nutrition != null && nutrition.totalFat!! >= 15 &&
-                nutrition.carbohydrates!! < 10 &&
-                nutrition.sugar!! < 5 && nutrition.protein!! in 10.0..30.0&&meal !in alreadySuggestKetoMeals
+        return nutrition?.totalFat != null && nutrition.totalFat >= MIN_FAT &&
+                nutrition.carbohydrates != null && nutrition.carbohydrates < MAX_CARBS &&
+                nutrition.sugar != null && nutrition.sugar < MAX_SUGAR &&
+                nutrition.protein != null && nutrition.protein in PROTEIN_RANGE&&
+                meal !in alreadySuggestKetoMeals
 
     }
+
+    companion object {
+        const val MIN_FAT = 15
+        const val MAX_CARBS = 10
+        const val MAX_SUGAR = 5
+        val PROTEIN_RANGE = 10.0..30.0}
 }
