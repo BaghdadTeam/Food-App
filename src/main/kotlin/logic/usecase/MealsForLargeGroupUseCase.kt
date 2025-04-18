9fc7c706198e0488433565daa778acd34c4bdf01
Aboud <gh.abd.com.207@gmail.com>
2025-04-18 13:39:08 -0700

    add customized exceptions for empty meal in meal provider and no elemnt match and handle it in ui

--- src/main/kotlin/logic/usecase/MealsForLargeGroupUseCase.kt
+++ src/main/kotlin/logic/usecase/MealsForLargeGroupUseCase.kt
@@ -2,16 +2,20 @@ package logic.usecase
 
 import logic.MealsProvider
 import model.Meal
+import org.example.utils.EmptyMeals
+import org.example.utils.NoElementMatch
 
 class MealsForLargeGroupUseCase(private val mealsProvider: MealsProvider) {
 
     private val countryName = "Italy"
     private val query = "Large group"
-    fun execute(): List<Meal> =
-        mealsProvider.getMeals()
+    fun execute(): List<Meal> {
+        if (mealsProvider.getMeals().isEmpty()) throw EmptyMeals("No meals found")
+        return mealsProvider.getMeals()
             .filter { grtMealsContainInput(it, countryName) && grtMealsContainInput(it, query) }
             .takeIf { it.isNotEmpty() }
-            ?: throw NoSuchElementException("There is no item found")
+            ?: throw NoElementMatch("There is no item found")
+    }
 
     private fun grtMealsContainInput(meal: Meal, input: String): Boolean {
         return (meal.name?.contains(input, ignoreCase = true) == true
