662cbab2687b48ee93edc4a45d007d323109c871
Aboud <gh.abd.com.207@gmail.com>
2025-04-18 13:29:54 -0700

    add customized exceptions for empty meal in meal provider and no elemnt match and handle it in ui

--- src/main/kotlin/logic/usecase/SuggestHighCalorieMealUseCase.kt
+++ src/main/kotlin/logic/usecase/SuggestHighCalorieMealUseCase.kt
@@ -2,18 +2,20 @@ package logic.usecase
 
 import model.Meal
 import logic.MealsProvider
+import org.example.utils.EmptyMeals
+import org.example.utils.NoElementMatch
 
 class SuggestHighCalorieMealUseCase(private val mealsProvider: MealsProvider) {
     private val suggestedMeals = mutableSetOf<Meal>()
 
     fun execute(): Meal {
-
+        if (mealsProvider.getMeals().isEmpty()) throw EmptyMeals("No meals found")
         return mealsProvider.getMeals()
             .filter(::isHighCalorieMeal)
             .takeIf { it.isNotEmpty() }
             ?.random()
             ?.also { suggestedMeals.add(it) }
-            ?: throw NoSuchElementException("There is no High Calories meals")
+            ?: throw NoElementMatch("There is no High Calories meals")
     }
 
     private fun isHighCalorieMeal(meal: Meal): Boolean =
