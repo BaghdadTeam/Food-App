4fcccfce600c74425d6e689245ae19052a908be4
Dunya4mars <rudina4mars@gmail.com>
2025-04-18 21:35:19 +0300

    add gym meal helper class

--- src/main/kotlin/logic/usecase/GymMealHelperUseCase.kt
+++ src/main/kotlin/logic/usecase/GymMealHelperUseCase.kt
@@ -1,6 +1,28 @@
-package org.example.logic.usecase
-
 import logic.MealsProvider
+import model.Meal
 
-class GymMealHelperUseCase(private  val mealProvider: MealsProvider) {
+class GymMealHelperUseCase(private val mealProvider: MealsProvider) {
+
+    fun getGymMealsSuggestion(
+        targetCalories: Int = 0,
+        targetProtein: Double = 0.0,
+        approximateTarget: Int = 50
+    ): List<Meal> {
+        return mealProvider.getMeals()
+            .filter { isSuitableGymMeal(it, targetCalories, targetProtein, approximateTarget) }
+    }
+
+    private fun isSuitableGymMeal(
+        meal: Meal,
+        targetCalories: Int,
+        targetProtein: Double,
+        range: Int
+    ): Boolean {
+        val nutrition = meal.nutrition ?: return false
+        val calories = nutrition.calories ?: return false
+        val protein = nutrition.protein ?: return false
+
+        return calories in (targetCalories - range).toFloat()..(targetCalories + range).toFloat() &&
+                protein in (targetProtein - range)..(targetProtein + range)
+    }
 }
--- src/main/kotlin/presentation/GymHelperUI.kt
+++ src/main/kotlin/presentation/GymHelperUI.kt
@@ -1,17 +1,48 @@
-package org.example.presentation
+package presentation.feature
+
+import GymMealHelperUseCase
+import org.example.presentation.Feature
+import org.example.presentation.SearchFoodByDateUI
 
 class GymHelperUI(
-//    private val useCase: GymHelperUseCase
+
+    private val useCase: GymMealHelperUseCase
 ) : Feature {
-    override val id: Int = FEATURE_ID
-    override val name: String = FEATURE_NAME
+    override val id: Int = SearchFoodByDateUI.FEATURE_ID
+    override val name: String = SearchFoodByDateUI.FEATURE_NAME
 
     override fun execute() {
-        // Implement your logic here
+
+        try {
+            print(" Enter desired calories: ")
+            val calories =
+                readlnOrNull()?.toIntOrNull() ?: throw Exception("Invalid number")
+
+            print(" Enter desired protein (g): ")
+            val protein =
+                readlnOrNull()?.toDoubleOrNull() ?: throw IllegalArgumentException("Invalid number")
+
+            val matchingMeals = useCase.getGymMealsSuggestion(calories, protein, 30)
+
+            if (matchingMeals.isEmpty()) {
+                println("No meals found matching your nutritional goals.")
+            } else {
+                println("Meals matching your gym goals:")
+                matchingMeals.forEachIndexed { index, meal ->
+                    println("${index + 1}. ${meal.name} - Calories: ${meal.nutrition?.calories}, Protein: ${meal.nutrition?.protein}g")
+                }
+            }
+
+        } catch (e: Throwable) {
+            println("Error: ${e.message}")
+        }
     }
 
     companion object {
         const val FEATURE_ID = 9
-        const val FEATURE_NAME = "Gym Helper"
+        const val FEATURE_NAME = "Gym helper"
     }
 }
+
+
+
