751d1f1331824980cbb8dde6a4507db17da379e8
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-04-18 11:01:48 +0200

    refactor: streamline display logic for random potato meals

--- src/main/kotlin/presentation/PotatoLovingMealsUI.kt
+++ src/main/kotlin/presentation/PotatoLovingMealsUI.kt
@@ -10,37 +10,37 @@ class PotatoLovingMealsUI(
     override val name: String = FEATURE_NAME
 
     override fun execute() {
-        fun displayRandomPotatoMeals(potatoMeals: List<Meal>) {
-            if (potatoMeals.isEmpty()) {
-                println("No potato meals found in the Dataset!")
-                return
-            }
+        val potatoMeals = useCase.execute()
+        if (potatoMeals.isEmpty()) {
+            println("No potato meals found in the Dataset!")
+            return
+        }
 
-            println("=== 10 Random Potato Meals ===")
-            println("Total potato meals found: ${potatoMeals.size}\n")
+        println("=== 10 Random Potato Meals ===")
+        println("Total potato meals found: ${potatoMeals.size}\n")
 
-            val randomMeals = if (potatoMeals.size > NUMBER_OF_POTATO_MEALS) {
-                potatoMeals.shuffled().take(NUMBER_OF_POTATO_MEALS)
-            } else {
-                potatoMeals.shuffled()
-            }
+        val randomMeals = if (potatoMeals.size > NUMBER_OF_POTATO_MEALS) {
+            potatoMeals.shuffled().take(NUMBER_OF_POTATO_MEALS)
+        } else {
+            potatoMeals.shuffled()
+        }
 
-            randomMeals.forEach { meal ->
-                println(
-                    """
+        randomMeals.forEach { meal ->
+            println(
+                """
             ID: ${meal.id}
             Name: ${meal.name}
             Description: ${meal.description!!.take(MAX_DESCRIPTION_LETTERS)}${if (meal.description.length > MAX_DESCRIPTION_LETTERS) "..." else ""}
             Calories: ${meal.nutrition?.calories ?: "N/A"}
             Ingredients: ${
-                        meal.ingredients!!.take(MAX_INGREDIENTS_NUMBER).joinToString()
-                    }${if (meal.ingredients.size > MAX_INGREDIENTS_NUMBER) "..." else ""}
+                    meal.ingredients!!.take(MAX_INGREDIENTS_NUMBER).joinToString()
+                }${if (meal.ingredients.size > MAX_INGREDIENTS_NUMBER) "..." else ""}
             ------------------------------------------
         """.trimIndent()
-                )
-            }
+            )
         }
     }
+
 
     companion object {
         const val FEATURE_ID = 12
