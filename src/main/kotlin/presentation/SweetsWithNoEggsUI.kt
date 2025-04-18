536a9935ba196d5d4888d02f59219679f0966c40
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-04-18 15:50:17 +0200

    Revert "refactor: cache meals in DefaultMealsProvider for improved performance"
    This reverts commit 1b72531b8bd85a898283285c2208fa40389e74d0.

--- src/main/kotlin/presentation/SweetsWithNoEggsUI.kt
+++ src/main/kotlin/presentation/SweetsWithNoEggsUI.kt
@@ -1,33 +1,13 @@
 package org.example.presentation
 
 import logic.usecase.EggFreeSweetsUseCase
-import org.example.utils.MealPresenter
 
 class SweetsWithNoEggsUI(private val useCase: EggFreeSweetsUseCase) : Feature {
     override val id: Int = FEATURE_ID
     override val name: String = FEATURE_NAME
 
     override fun execute() {
-        try {
-            while (true) {
-                val sweet = useCase.execute()
-                println("How about this sweet: ${sweet.name}")
-                print("Do you like it? (yes/no): ")
-                val choice = readln().trim()
-                if (choice.contains("y", ignoreCase = true)) {
-                    MealPresenter.printDetails(meal = sweet)
-                    break
-                }
-            }
-        } catch (e: NoSuchElementException) {
-            println("There is no egg free sweets found")
-        } catch (e: Exception) {
-            println(
-                """Something wrong happened when retrieving the data.
-                |please try again later
-            """.trimMargin()
-            )
-        }
+        println("Suggested Sweet: ${useCase.execute()}")
     }
 
     companion object {
