42cf45fab0f33db602601bba29954a60e123e399
Aboud <gh.abd.com.207@gmail.com>
2025-04-18 13:37:21 -0700

    add customized exceptions for empty meal in meal provider and no elemnt match and handle it in ui

--- src/main/kotlin/logic/usecase/FilterQuickHealthyMealsUseCase.kt
+++ src/main/kotlin/logic/usecase/FilterQuickHealthyMealsUseCase.kt
@@ -1,18 +1,21 @@
 package logic.usecase
 
-import model.Meal
 import logic.MealsProvider
+import model.Meal
+import org.example.utils.EmptyMeals
+import org.example.utils.NoElementMatch
 
 class FilterQuickHealthyMealsUseCase(private val mealsProvider: MealsProvider) {
 
-
     fun execute(count: Int): List<Meal> {
+        if (mealsProvider.getMeals().isEmpty()) throw EmptyMeals("No meals found")
+
         return mealsProvider.getMeals()
             .filter(::isQuickAndHasNutrition)
             .sortedBy(::healthScore)
             .take(count)
             .takeIf { it.isNotEmpty() }
-            ?: throw NoSuchElementException("There is no more healthy meals")
+            ?: throw NoElementMatch("There is no more healthy meals")
     }
 
     private fun isQuickAndHasNutrition(meal: Meal): Boolean {
@@ -22,11 +25,12 @@ class FilterQuickHealthyMealsUseCase(private val mealsProvider: MealsProvider) {
     private fun healthScore(meal: Meal): Double {
         val nutrition = meal.nutrition!!
         return (nutrition.totalFat ?: 0.0) +
-                (nutrition.saturatedFat ?:0.0) +
+                (nutrition.saturatedFat ?: 0.0) +
                 (nutrition.carbohydrates ?: 0.0)
     }
 
     companion object {
         const val MAX_PREPARATION_TIME = 15
-        
-}}
+
+    }
+}
--- src/main/kotlin/presentation/QuickHealthyMealsUI.kt
+++ src/main/kotlin/presentation/QuickHealthyMealsUI.kt
@@ -1,6 +1,8 @@
 package org.example.presentation
 
 import logic.usecase.FilterQuickHealthyMealsUseCase
+import org.example.utils.EmptyMeals
+import org.example.utils.NoElementMatch
 
 class QuickHealthyMealsUI(private val useCase: FilterQuickHealthyMealsUseCase) : Feature {
     override val id: Int = FEATURE_ID
@@ -22,7 +24,9 @@ class QuickHealthyMealsUI(private val useCase: FilterQuickHealthyMealsUseCase) :
                     println("Quick & Healthy Meals:")
                     meals.forEach { println("- ${it.name}") }
                 }
-            } catch (e: NoSuchElementException) {
+            } catch (e: EmptyMeals) {
+                println("There is no meals in database")
+            } catch (e: NoElementMatch) {
                 println("There are no quick healthy meals available ")
             } catch (e: Exception) {
                 println("There is a problem happened when retrieving the data.")
