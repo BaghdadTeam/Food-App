db963c358353d8c6fc0f702cb2dbafd4b53a20ae
haider <haideraljawasim@gmail.com>
2025-04-22 14:02:21 +0300

    add test case for suggest keto meal use case

--- src/test/kotlin/logic/usecase/SuggestKetoMealUseCaseTest.kt
+++ src/test/kotlin/logic/usecase/SuggestKetoMealUseCaseTest.kt
@@ -1,4 +1,27 @@
-import org.junit.jupiter.api.Assertions.*
+package logic.usecase
+
+import io.mockk.every
+import io.mockk.mockk
+import logic.MealsProvider
+import org.example.utils.EmptyMealsException
+
+import org.junit.jupiter.api.*
+
 class SuggestKetoMealUseCaseTest {
-  
+    private lateinit var mealProvider: MealsProvider
+    private lateinit var useCase: SuggestKetoMealUseCase
+
+
+    @BeforeEach
+    fun setUp() {
+        mealProvider = mockk(relaxed = true)
+        useCase = SuggestKetoMealUseCase(mealProvider)
+    }
+    @Test
+    fun `Should throw EmptyMealsException when there is no meals to filler`() {
+        // Given
+        every { mealProvider.getMeals() }.returns(emptyList())
+        // When & Then
+        assertThrows<EmptyMealsException> { useCase.execute() }
+    }
 }
