3377aca9b3c2c6b49c52d098157e0908a972236f
Aboud <gh.abd.com.207@gmail.com>
2025-04-23 11:02:18 -0700

    implements test cases for SearchMealUseCase.kt

---
+++ src/test/kotlin/logic/usecase/SearchMealUseCaseTest.kt
@@ -0,0 +1,105 @@
+package logic.usecase
+
+import com.google.common.truth.Truth.assertThat
+import io.mockk.every
+import io.mockk.mockk
+import logic.MealsProvider
+import logic.helpers.createMealHelper
+import org.example.logic.search.KMPSearchAlgorithm
+import org.example.utils.EmptyMealNameException
+import org.example.utils.EmptyMealsException
+import org.example.utils.NoMealFoundException
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SearchMealUseCaseTest {
+    private lateinit var mealsProvider: MealsProvider
+    private lateinit var searchMeal: SearchMealUseCase
+    private lateinit var kMPSearchAlgorithm: KMPSearchAlgorithm
+
+    @BeforeEach
+    fun setup() {
+        kMPSearchAlgorithm = KMPSearchAlgorithm()
+        mealsProvider = mockk(relaxed = true)
+        searchMeal = SearchMealUseCase(mealsProvider, kMPSearchAlgorithm)
+    }
+
+
+
+    @Test
+    fun `should throw exception when MealsProvider returns empty list`() {
+        // Given
+        every { mealsProvider.getMeals() } returns emptyList()
+        // When
+        assertThrows<EmptyMealsException> {searchMeal.execute("aboud")}
+    }
+
+
+    @Test
+    fun `should throw exception when empty meal name input`() {
+        // Given
+        every { mealsProvider.getMeals() } returns listOf(
+            createMealHelper(name = "pizza")
+        )
+        // When & Test
+        assertThrows<EmptyMealNameException> {searchMeal.execute("")}
+    }
+
+    @Test
+    fun `Should return Meal when the Meal is found`() {
+        // Given
+        every { mealsProvider.getMeals() } returns listOf(
+            createMealHelper(name = "pizza")
+        )
+        // When
+        val meal = searchMeal.execute("pizza")
+
+        // Then
+        assertThat(meal[0].name).isEqualTo("pizza")
+
+    }
+
+
+    @Test
+    fun `should throw an exception when no meal found`() {
+        // Given
+        every { mealsProvider.getMeals() } returns listOf(
+            createMealHelper(name = "meal")
+        )
+        // When & Test
+        assertThrows<NoMealFoundException> {searchMeal.execute("pizza")}
+    }
+
+    @Test
+    fun `should return meal when case insensitive search`() {
+        // Given
+        every { mealsProvider.getMeals() } returns listOf(
+            createMealHelper(name = "Pizza")
+        )
+        // When
+        val meal = searchMeal.execute("pizza")
+
+        // Then
+        assertThat(meal[0].name).isEqualTo("Pizza")
+
+    }
+
+    @Test
+    fun `should return multiple meals when multiple meals founds`() {
+        // Given
+        every { mealsProvider.getMeals() } returns listOf(
+            createMealHelper(name = "Pizza"),
+            createMealHelper(name = "Aboud Pizza")
+
+        )
+        // When
+        val meal = searchMeal.execute("pizza")
+
+        // Then
+        assertThat(meal.size).isEqualTo(2)
+    }
+
+
+}
