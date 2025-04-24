61edc918534892f0f8319fb0a7c415cf93a2a815
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-04-24 16:20:43 +0200

    add CsvParsingException and CsvLoadingException

--- src/main/kotlin/utils/Exceptions.kt
+++ src/main/kotlin/utils/Exceptions.kt
@@ -1,6 +1,8 @@
 package org.example.utils
 
-class EmptyMealNameException(message : String = "")  : Exception(message)
-class EmptyMealsException(message : String = "")  : Exception(message)
-class NoMealFoundException(message : String = "")  : NoSuchElementException(message)
+class EmptyMealNameException(message: String = "") : Exception(message)
+class EmptyMealsException(message: String = "") : Exception(message)
+class NoMealFoundException(message: String = "") : NoSuchElementException(message)
 class InvalidDateFormatException(message: String = "") : Exception(message)
+class CsvParsingException(message: String = "", cause: Throwable) : Exception(message, cause)
+class CsvLoadingException(message: String = "", cause: Throwable) : Exception(message, cause)
