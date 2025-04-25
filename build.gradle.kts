c2e873fd942ccfab8df66cda580cebdf4818679f
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-04-25 16:02:12 +0300

    add JaCoCo test coverage enforcement and configure reporting

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,13 +30,41 @@ dependencies {
 
     // mockk
     testImplementation("io.mockk:mockk:1.14.0")
+
     // junit params
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.2")
 }
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
 }
+
+tasks.jacocoTestReport {
+    reports {
+        csv.required.set(false)
+        xml.required.set(true)
+        html.required.set(true)
+    }
+    dependsOn(tasks.test)
+}
+
+tasks.jacocoTestCoverageVerification {
+    executionData.setFrom(fileTree(buildDir).include("/jacoco/test.exec"))
+    classDirectories.setFrom(fileTree("build/classes/kotlin/main/logic/usecase"))
+    sourceDirectories.setFrom(files("src/main/kotlin/logic/usecase"))
+    violationRules {
+        rule {
+            limit {
+                counter = "CLASS"
+                value = "COVERED RATIO"
+                minimum = "1.0".toBigDecimal() // 100% coverage
+
+            }
+        }
+    }
+}
+
 kotlin {
     jvmToolchain(20)
 }
