5fdd9db6b469773e0ae93f3838e3bd3514465b67
Aboud <gh.abd.com.207@gmail.com>
2025-04-25 02:13:22 -0700

    change package path to logic.usecase.search

---
+++ src/main/kotlin/logic/usecase/search/SearchMealUseCase.kt
@@ -0,0 +1,30 @@
+package logic.usecase.search
+
+import logic.MealsProvider
+import model.Meal
+import org.example.logic.SearchAlgorithm
+import org.example.utils.EmptyMealNameException
+import org.example.utils.EmptyMealsException
+import org.example.utils.EmptyTextException
+import org.example.utils.NoMealFoundException
+
+class SearchMealUseCase(
+    private val mealsProvider: MealsProvider,
+    private val searchAlgorithm: SearchAlgorithm
+) {
+
+    fun execute(name: String): List<Meal> {
+        if (mealsProvider.getMeals().isEmpty()) throw EmptyMealsException("No meals found")
+        if (name.isBlank() or name.isEmpty()) throw EmptyMealNameException("Meal name should not be empty")
+
+        return mealsProvider.getMeals().filter { meal ->
+            if (meal.name.isNullOrEmpty()) throw EmptyTextException("empty meal name in database")
+            if (meal.name.isBlank()) throw EmptyTextException("empty meal name in database")
+
+            searchAlgorithm.search(meal.name.lowercase(), name.lowercase())
+        }
+            .takeIf { it.isNotEmpty() }
+            ?: throw NoMealFoundException("No meals found matching '$name'.")
+    }
+}
+
